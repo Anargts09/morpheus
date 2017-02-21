2788d915327103e7b8f1fc63507d4d1a1f8384e3
Anargts09 <anargts0209@gmail.com>
2017-02-21 14:05:05 +0800

    'first'

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+    let $hoge = $('.hoge');
+    let isFormation = 'dia';
+ 
+    let hoge = new SVGMorpheus('.hoge', {
+        duration: '500',
+        easing: 'quint-in-out',
+        rotation: 'clock'
+    });
+ 
+    setInterval(()=>{
+        if(isFormation === 'dia'){
+            hoge.to('HEART');
+            isFormation = 'heart';
+        }else if(isFormation === 'heart'){
+            hoge.to('CLOVER');
+            isFormation = 'clover';
+        }else if(isFormation === 'clover'){
+            hoge.to('SPADE');
+            isFormation = 'spade';
+        }else if(isFormation === 'spade'){
+            hoge.to('DIA');
+            isFormation = 'dia';
+        }
+    }, 2500);
+});
